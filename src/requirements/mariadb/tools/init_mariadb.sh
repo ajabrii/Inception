af8dbff420739f345c426b0b7d2d4eca91fc87fb
neo <phoenixali0@gmail.com>
2025-06-19 17:57:44 +0000

    mariadb setup is 90% done remember keep your .env in .gitignore file (later on for now it's ok)

---
+++ src/requirements/mariadb/tools/init_mariadb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+service mariadb start
+sleep 3
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
