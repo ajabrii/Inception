2f125ae10eaca16f10d9e5dd8a3e28b7ec262536
ajabri <ajabri@debian.myguest.virtualbox.org>
2025-08-22 12:56:06 +0100

    push

--- src/requirements/bonus/ftp/tools/start.sh
+++ src/requirements/bonus/ftp/tools/start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-useradd -m -d /var/www/wordpress $FTP_USER
-echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+useradd -m -d /var/www/wordpress $FTP_USR
+echo "$FTP_USR:$FTP_PWD" | chpasswd
+
+# Add FTP user to www-data group
+usermod -aG www-data $FTP_USR
+
+# Ensure wordpress files are owned by www-data
+chown -R www-data:www-data /var/www/wordpress
+
+# Group needs write perms
+chmod -R 775 /var/www/wordpress
 
 exec vsftpd /etc/vsftpd.conf
